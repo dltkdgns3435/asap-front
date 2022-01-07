f216d1846ead0aaee6f2989a36b3757412eb333d
sh-mac <dltkdgns3435@naver.com>
2022-01-07 09:45:07 +0900

    #7: front <-> rest 연동

--- src/main/java/com/worryswat/asap/controller/StatusController.java
+++ src/main/java/com/worryswat/asap/controller/StatusController.java
@@ -1,16 +1,24 @@
 package com.worryswat.asap.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/status")
 public class StatusController {
+    private final String restUri = "http://localhost:8081/";
+    private final RestTemplate restTemplate;
 
     @GetMapping("/server")
     public String serverStatus(){
-        return "ok";
+        String uri = restUri + "status/server";
+        String response = restTemplate.getForObject(uri, String.class);
+
+        return "ok / "+response;
     }
 
 }
