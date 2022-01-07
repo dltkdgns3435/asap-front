8279d84e0fc19ddfd239f506b84f752fe7c69aae
sh-mac <dltkdgns3435@naver.com>
2022-01-07 10:45:48 +0900

    #8: local, prod 환경 분리

--- src/main/java/com/worryswat/asap/controller/StatusController.java
+++ src/main/java/com/worryswat/asap/controller/StatusController.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/status")
 public class StatusController {
 
-    @Value("${spring.rest.uri}")
+    @Value("${rest.uri}")
     private String restUri;
     private final RestTemplate restTemplate;
 
